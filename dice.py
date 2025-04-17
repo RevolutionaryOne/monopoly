58888a2b24b8ed74fe8fb65728dea2aba3d3e778
RevolutionaryOne <mrlhowarth@gmail.com>
2025-04-17 17:02:03 +0100

    Updated dice function

--- dice.py
+++ dice.py
@@ -3,8 +3,8 @@ import player
 import board
 
 def dice_roll(player):
-    dice_1 = random.randint(1, 2)
-    dice_2 = random.randint(1, 2)
+    dice_1 = random.randint(1, 6)
+    dice_2 = random.randint(1, 6)
     total = dice_1 + dice_2
 
     if dice_1 == dice_2:
