e3bf2df5ef9497495ccf89550b31223cb57e4aed
RevolutionaryOne <mrlhowarth@gmail.com>
2025-04-17 15:13:01 +0100

    Initial comment

---
+++ monopoly_main.py
@@ -0,0 +1,10 @@
+'''This is a text based monopoly game based on the Falmouth version of Monopoly. 
+During setup the player can select a range of game options, standard rules, 
+standard rules less the auction each turn rule and the final option is to collect fines/taxes into a pot that can be won if 
+a player lands on free parking.'''
+
+import random
+import board
+import player
+
+print("Welcome to Monopoly!")
