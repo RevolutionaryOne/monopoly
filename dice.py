0f61366e1044fc1f99f2e7cd3e2322d9f6fbc95d
RevolutionaryOne <mrlhowarth@gmail.com>
2025-04-17 16:34:21 +0100

    Added initial dice logic

---
+++ dice.py
@@ -0,0 +1,26 @@
+import random
+
+def dice_roll(player: Player, double_counter: int):
+    dice_1 = random.randint(1, 6)
+    dice_2 = random.randint(1, 6)
+    total = dice_1 + dice_2
+
+    if dice_1 == dice_2:
+        player.double_counter += 1
+        print(f"You rolled a double {dice_1}!")
+        print("Move", total, "spaces.") 
+        # Move the player the number of spaces rolled
+        # Carry out game logic for that space
+        # Return for next role
+        # Check if the player has rolled 3 doubles in a row - if so, go to jail
+        if player.double_counter == 3:
+            print("You rolled 3 doubles in a row! Go to jail.")
+            # Move player to jail
+    else:
+        player.double_counter = 0 # Reset the double counter if previous roll increased it
+        print(dice_1, "and", dice_2)
+        print("Move", total, "spaces.") 
+        # Move the player the number of spaces rolled
+
+
+    
--- monopoly_main.py
+++ monopoly_main.py
@@ -1,28 +1,14 @@
+import board
+import player
+import ascii_art
+import dice
 '''This is a text based monopoly game based on the Falmouth version of Monopoly. 
 During setup the player can select a range of game options, standard rules, 
 standard rules less the auction each turn rule and the final option is to collect fines/taxes into a pot that can be won if 
 a player lands on free parking.'''
 
-import random
-import board
-import player
-import ascii_art
-
 # Print the main menu ASCII art
 ascii_art.monopoly_ascii()
 
-player_name = input("Please enter your name: ")
-print("\n")
-print("Welcome to Monopoly, " + player_name + "!")
-print("\n")
-number_of_players = int(input("Please enter the number of players (2-6): "))
-print("\n")
-print(f"Great! You have selected {number_of_players} players.")
-print("\n")
-
-dice_1 = new.random.randint(1, 6)
-dice_2 = new.random.randint(1, 6)
-
-dice_roll = dice_1 + dice_2
-
-print(f"You rolled a {dice_1} and a {dice_2}.")
+# Test dice roll function
+dice.dice_roll()
